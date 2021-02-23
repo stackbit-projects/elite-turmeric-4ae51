bc02acdae4b8916bb99647c80e9807657b59647a
Stackbit Code Editor <support@stackbit.com>
2021-02-23 07:49:28 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,7 +73,7 @@ const IndexPage = () => (
                 fontSize={[0, 1, 2]}
                 href="#"
               >
-                customize
+                shell function
               </LinkLight>{" "}
               ・
               <LinkLight
