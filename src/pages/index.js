bb51e135a426d261d4fede4622264e0fe6063b92
Stackbit Code Editor <support@stackbit.com>
2021-02-23 07:45:08 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -224,7 +224,7 @@ const IndexPage = () => (
           py={5}
         >
           <Text pr={1} as="span" fontSize={2} fontWeight={500}>
-            HardHat is &#8217;s
+            HardHat is devloped and maintained by&#8217;s
           </Text>
           <LinkDark
             display="inline-block"
